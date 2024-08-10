284e33e6abd11f88375e6a7a003b7609e76b363e
dhananjayadasacharya <dhananjayadasacharya@gmail.com>
2024-08-10 13:42:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Welcome to my 1st website ig?")
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZche017hQq3YjO4Z7InsCghwAqoaY2_0",
+  authDomain: "dummydas1-ad6a2.firebaseapp.com",
+  projectId: "dummydas1-ad6a2",
+  storageBucket: "dummydas1-ad6a2.appspot.com",
+  messagingSenderId: "947305466855",
+  appId: "1:947305466855:web:8343c8b630597ff26d0b42"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Good");
